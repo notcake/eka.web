c71eed7cff29e1a76314ee8233f76a0d27dea791
PotcFdk <PotcFdk@gmx.net>
2013-12-10 20:47:58 +0100

    Fixed Amazon code.

--- Amazon/Amazon.cs
+++ Amazon/Amazon.cs
@@ -15,10 +15,13 @@ namespace Eka.Web.Amazon
         public string Data { get; protected set; }
 
         public string Uri { get; protected set; }
+        public string ListingUri { get; protected set; }
+
+        public string Tld { get; protected set; }
+        public string Id { get; protected set; }
 
         public string Title { get; protected set; }
         public string Price { get; protected set; }
-        public string Currency { get; protected set; }
 
         public Amazon(string uri)
         {
@@ -41,29 +44,53 @@ namespace Eka.Web.Amazon
             if (match.Success)
             {
                 this.Title = match.Groups[1].ToString();
+                this.Success = true;
             }
             else
             {
                 return;
             }
 
-            //match = new Regex("<td><b class=\"priceLarge\">(.+)</b>", RegexOptions.IgnoreCase).Match(this.Data);
-            match = new Regex("\"buyingPrice\":([0-9.]+)", RegexOptions.IgnoreCase).Match(this.Data);
+            // Disassemble URI
+
+            match = new Regex("https?://(www\\.)?amazon\\.(com|de|co\\.uk)([^ ]+)?/[dg]p/(product/)?([a-zA-Z0-9]+)", RegexOptions.IgnoreCase).Match(this.Data);
+            if (match.Success)
+            {
+                this.Tld = match.Groups[2].ToString();
+                this.Id = match.Groups[5].ToString();
+            }
+            else
+            {
+                return;
+            }
+
+            // Assemble listing uri / Download listings
+
+            this.ListingUri = "http://www.amazon." + this.Tld + "/gp/offer-listing/" + this.Id;
+
+            try
+            {
+                WebClient web = new WebClient();
+                web.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+                this.Data = web.DownloadString(this.ListingUri);
+            }
+            catch (WebException e)
+            {
+                this.Price = e.Message;
+                return;
+            }
+
+            match = new Regex("<li id=\"olpTabNew\".+?href.+?>(.+?)</a></li>", RegexOptions.IgnoreCase).Match(this.Data);
+
             if (match.Success)
             {
                 this.Price = match.Groups[1].ToString();
+                this.Price = Regex.Replace(this.Price, "<.*?>", string.Empty);
                 //this.Price = Double.Parse(match.Groups[1].ToString(), );
             }
             else
             {
                 return;
-            }
-
-            match = new Regex("\"currencyCode\":\"([a-zA-Z]+)\",", RegexOptions.IgnoreCase).Match(this.Data);
-            if (match.Success)
-            {
-                this.Currency = match.Groups[1].ToString();
-                this.Success = true;
             }
         }
     }
