d0c2a6af46553e2b34c3fb9e3645e54badbb56f8
PotcFdk <PotcFdk@gmx.net>
2014-01-19 21:07:27 +0100

    Added PlotGenerator.

---
+++ PlotGenerator/Plot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace Eka.Web.PlotGenerator
+{
+    public class Plot
+    {
+        public bool Success { get; protected set; }
+
+        public string PlotText { get; protected set; }
+
+        public string Uri { get; protected set; }
+
+        public Plot()
+        {
+            this.Success = false;
+            string Data;
+
+            this.Uri = "https://words.bighugelabs.com/plot.php";
+
+            // Get raw data
+            try
+            {
+                Data = new WebClient().DownloadString(this.Uri);
+            }
+            catch (WebException)
+            {
+                return;
+            }
+
+            Match match = new Regex("<ul class=\"loglines\"><li>(.+?)</li>", RegexOptions.IgnoreCase).Match(Data);
+
+            if (match.Success)
+            {
+                this.PlotText = match.Groups[1].ToString();
+                this.Success = true;
+            }
+        }
+    }
+}
