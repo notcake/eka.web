ce4441c413a23a6e55810d257dfb36fc0da9fd9a
Techbot121 <techbot121@gmail.com>
2014-12-06 21:32:38 +0100

    (tried) to add Wolfram

---
+++ Wolfram/Wolfram.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Xml;
+
+namespace Eka.Web.Wolfram
+{
+    public class Wolfram
+    {
+        public bool Success { get; protected set; }
+
+        public string Uri { get; protected set; }
+        public string Query { get; protected set; }
+        public string Data { get; protected set; }
+        public string Output { get; protected set; }
+
+        public Wolfram(string Query)
+        {
+            this.Success = false;
+            this.Query = System.Uri.EscapeDataString(Query);
+
+            this.Uri = "http://api.wolframalpha.com/v2/query?input=" + this.Query + "&appid=TY5Q8Y-UVE96HQA92";
+
+            try
+            {
+                this.Data = new WebClient().DownloadString(this.Uri);
+            }
+            catch (WebException)
+            {
+                return;
+            }
+            this.Success = true;
+
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.LoadXml(this.Data);
+            }
+            catch (XmlException) { }
+
+            XmlNode queryresult = doc.SelectSingleNode("//queryresult");
+            if (queryresult == null) { return; }
+            bool success = Convert.ToBoolean(queryresult.Attributes["success"].Value);
+
+            if (success)
+            {
+                XmlNodeList pods = queryresult.SelectNodes("//pod");
+                if (pods.Count < 1 | pods[1] == null) { this.Output = "got nothing, database down?"; return; }
+
+                this.Output = pods[0]["subpod"]["plaintext"].InnerText +"\n\n"+ pods[1]["subpod"]["plaintext"].InnerText.Replace("|","->");
+
+            }
+            else
+            {
+                XmlNodeList rest = queryresult.SelectNodes("*");
+                if (rest[0].Name == "didyoumeans")
+                {
+                    this.Output = "Did You mean? "+rest[0].InnerText+"?";
+                }
+                else if (rest[0].Name == "tips")
+                {
+                    this.Output = "Tip: " + rest[0].Attributes["text"].Value;
+                }
+                            
+            }
+        }
+
+    }
+}
