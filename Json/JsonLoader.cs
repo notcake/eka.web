27ef99e3a330b056c136ce819bb8765292e92466
Techbot <techbot121@gmail.com>
2015-05-11 13:25:56 +0200

    More dynamic approach to json, I'll try to port Youtube using the same technique.

---
+++ Json/JsonLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace Eka.Web.Json
+{
+    internal static class JsonLoader
+    {
+        public static object Load(string uri)
+        {
+            var magic = JsonConvert.DeserializeObject<dynamic>(new WebClient().DownloadString(uri));
+
+            return magic;
+        }
+    }
+}
